773140f69c4c7a154fbe02f507bd885d0e574ad7
sam <se.mcauley1@gmail.com>
2026-04-17 15:54:30 +0100

    make performance improvements

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ load_dotenv(find_dotenv())
 path = "https://api.carbonintensity.org.uk/intensity"
 headers = {"Accept": "application/json"}
 
-def extract_one_week():
-    from_date = "2026-04-09T15:00Z"
-    to_date = "2026-04-16T15:00Z"
+def extract_intensity_date_range():
+    from_date = os.environ.get("BACKFILL_START_DATE")
+    to_date = os.environ.get("BACKFILL_END_DATE")
 
     logger.info(f"Fetching data from {from_date} to {to_date}")
 
@@ -37,22 +37,21 @@ def add_row_to_db(data, conn):
         %s, %s, %s, %s, %s, NOW()
     ) ON CONFLICT (from_ts, to_ts) DO NOTHING;
     """
+    rows = [
+        (
+            record["from"],
+            record["to"],
+            record["intensity"]["forecast"],
+            record["intensity"]["actual"],
+            record["intensity"]["index"]
+        )
+        for record in data
+    ]
     try:
         with conn.cursor() as cur:
-            cur.execute(query, (
-                data['from'],
-                data['to'],
-                data['intensity']['forecast'],
-                data['intensity']['actual'],
-                data['intensity']['index']
-            ))
-
+            cur.executemany(query, rows)
             conn.commit()
-
-            if cur.rowcount == 0:
-                logger.info("Duplicate skipped")
-            else:
-                logger.info("Row inserted")
+            logger.info(f"Inserted {cur.rowcount} rows, skipped duplicates.")
         
     except (Exception, psycopg2.DatabaseError) as error:
         logger.exception("Error inserting row")
@@ -62,17 +61,16 @@ def add_row_to_db(data, conn):
 
 if __name__ == "__main__":
     conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        dbname="carbonintensity",
-        user="sm",
+        host=os.environ.get("POSTGRES_HOST", "localhost"),
+        port=os.environ.get("POSTGRES_PORT", 5432),
+        dbname=os.environ.get("POSTGRES_DB"),
+        user=os.environ.get("POSTGRES_USER"),
         password=os.environ.get("POSTGRES_PASSWORD")
     )
 
     try:
-        data = extract_one_week()
+        data = extract_intensity_date_range()
         
-        for record in data:
-            add_row_to_db(record, conn)
+        add_row_to_db(data, conn)
     finally:
         conn.close()
